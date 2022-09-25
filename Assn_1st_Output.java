0e50b11f7696ea0d4fede60693e42e531387226f
Akhilpb15 <104380444+Akhilpb15@users.noreply.github.com>
2022-09-25 23:19:09 +0530

    Add files via upload

---
+++ Assn_1st_Output.java
@@ -0,0 +1,17 @@
+Enter the names of your family members along with their age (FORMAT: FirstName_Age): 
+Akhil_20
+Do you want to add more names. If yes press 1, else press any other number: 
+1
+Enter the names of your family members along with their age (FORMAT: FirstName_Age): 
+Tushant_90
+Do you want to add more names. If yes press 1, else press any other number: 
+1
+Enter the names of your family members along with their age (FORMAT: FirstName_Age): 
+Kartik_65
+Do you want to add more names. If yes press 1, else press any other number: 
+1
+Enter the names of your family members along with their age (FORMAT: FirstName_Age): 
+Arnav_9
+Do you want to add more names. If yes press 1, else press any other number: 
+3
+Arnav_9 Kartik_65 Tushant_90 AKhil_20
